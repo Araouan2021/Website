a42de23eeec503111d535be0165aa8d36127e383
Bola Bally <bolabally@gmail.com>
2020-06-02 14:44:03 +0000

    addblog.blade.php

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,18 +1,19 @@
-All of our products have numerous health benefits.
+<br>All of our products have numerous health benefits.<br>
 
-Although peanuts are high in fat, they contain monounsaturated and polyunsaturated fats which promote a healthy heart.
+<br>Although peanuts are high in fat, they contain monounsaturated and polyunsaturated fats which promote a healthy heart.<br>
 
-Peanuts, groundnuts or arachides contain magnesium, copper, and arginine which are believed to reduce the risk of cardiovascular disease.
+<br>Peanuts, groundnuts or arachides contain magnesium, copper, and arginine which are believed to reduce the risk of cardiovascular disease.<br>
 
-Since peanut skins are rich in phenolic compounds, they are also rich in antioxidants.
-Antioxidants protect the body from oxidative stress, which occurs in differrent types of cancers and diseases.
+<br>Since peanut skins are rich in phenolic compounds, they are also rich in antioxidants.<br>
+<br>Antioxidants protect the body from oxidative stress, which occurs in differrent types of cancers and diseases.<br>
 
-Antioxidants also prohibit and in some cases, prevent the oxidation of other molecules in the body.
-Peanuts are also a good source of plant protein.  Protein is needed to build and repair tissue.
+<br>Antioxidants also prohibit and in some cases, prevent the oxidation of other molecules in the body.
+Peanuts are also a good source of plant protein.  Protein is needed to build and repair tissue.<br>
 
-Peanuts are also rich in minerals like magnesium, phosphorus, potassium, zinc, calcium, sodium.  Minerals are needed for our bodies to function well.  In addition, an adequate supply of these minerals ensure a healthy heart and minimize the risk of mineral-deficient diseases.
+<br>Peanuts are also rich in minerals like magnesium, phosphorus, potassium, zinc, calcium, sodium.  Minerals are needed for our bodies to function well.  In addition, an adequate supply of these minerals ensure a healthy heart and minimize the risk of mineral-deficient diseases.<br>
 
-Good levels of monounsaturated and polyunsaturated fats help lower blood cholesterol levels as well as reduce the risk of coronary heart disease.
+<br>Good levels of monounsaturated and polyunsaturated fats help lower blood cholesterol levels as well as reduce the risk of coronary heart disease.<br>
 
-Lastly, peanuts are rich in vitamins like foliate and vitamin E.  Vitamins are essential for growth and development.
- www.pulse.ng
+<br>Lastly, peanuts are rich in vitamins like foliate and vitamin E.  Vitamins are essential for growth and development.<br>
+
+www.pulse.ng
