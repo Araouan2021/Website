897185a652becaa9a70cd28a68e44786f2e5abf8
Bola Bally <bolabally@gmail.com>
2020-06-04 17:52:45 +0000

    addblog.blade.php

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,11 +1,13 @@
 <br>All of our products have numerous health benefits.<br>
 
+<h3>PEANUTS (ARACHIDES)<h3>
+
 <br>Although peanuts are high in fat, they contain monounsaturated and polyunsaturated fats which promote a healthy heart.<br>
 
 <br>Peanuts, groundnuts or arachides contain magnesium, copper, and arginine which are believed to reduce the risk of cardiovascular disease.<br>
 
 <br>Since peanut skins are rich in phenolic compounds, they are also rich in antioxidants.<br>
-<br>Antioxidants protect the body from oxidative stress, which occurs in differrent types of cancers and diseases.<br>
+<br>Antioxidants protect the body from oxidative stress, which occurs in different types of cancers and diseases.<br>
 
 <br>Antioxidants also prohibit and in some cases, prevent the oxidation of other molecules in the body.
 Peanuts are also a good source of plant protein.  Protein is needed to build and repair tissue.<br>
@@ -17,3 +19,17 @@ Peanuts are also a good source of plant protein.  Protein is needed to build and
 <br>Lastly, peanuts are rich in vitamins like foliate and vitamin E.  Vitamins are essential for growth and development.<br>
 
 www.pulse.ng
+
+<h3>CASHEWNUTS (NOIX DE CAJOU)<h3>
+
+<br>Cashews are full of vitamins, minerals and antioxidants. For example, these include vitamins E, K, and B-6, and minerals like copper, phosphorus, zinc, magnesium, iron, and selenium.<br>
+
+<br>Some vitamins and minerals in nuts, such as potassium, vitamins E and B-6, and folic acid, help fight heart disease.<br>
+
+<br>Cashews contain copper and iron which work together to help the body form and use red blood cells. This in turn keeps blood vessels, nerves, bones, and the immune system healthy and functioning properly.<br>
+
+<br>Cashews also contain high levels of lutein and zeaxanthin, which act as antioxidants. When eaten daily, these antioxidant compounds can protect the eyes from the type of damage that can lead to blindness as we get older.<br>
+
+<br>Cashews contain a lot of "good fat" which is believed to help prevent heart disease and reduce the risk of stroke. The fat also helps you feel full and satisfied and may help in managing your weight.<br>
+
+wwww.healthline.com
