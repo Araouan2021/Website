b5d5ec067a94f94a073911641bed73c3e52c7429
Bola Bally <bolabally@gmail.com>
2020-06-17 09:36:53 +0000

    addcreate_table_php

--- database/migrations/2020_05_04_133953_create_products_table.php
+++ database/migrations/2020_05_04_133953_create_products_table.php
@@ -15,8 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->string('price');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('details')->nullable();
+            $table->double('price');
+            $table->double('shipping_cost');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->unsignedInteger('brand_id')->unsigned();
+            $table->string('image_path');
             $table->timestamps();
         });
     }
