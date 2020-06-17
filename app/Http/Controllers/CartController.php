8761eb1408e1198afb7edb23a89eb5fd73a60945
Bola Bally <bolabally@gmail.com>
2020-06-17 13:11:50 +0000

    addCartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Product;
 
+use App\Cart;
+
 class CartController extends Controller
 {
     public function shop()
@@ -18,8 +20,22 @@ class CartController extends Controller
     public function cart()  {
         $cartCollection = \Cart::getContent();
         dd($cartCollection);
-        return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
+        return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);
+    }
+
+    public function add(Request$request){
+        \Cart::add(array(
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->img,
+                'slug' => $request->slug
+            )
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'Item is Added to Cart!');
     }
 
 }
-}
+
